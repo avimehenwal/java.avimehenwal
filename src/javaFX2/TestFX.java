2b188d55de9116518cc8f02988e1e41f8e518d34
avimehenwal <avi.mehanwal@gmail.com>
2020-06-22 12:45:22 +0200

    feat(scene): Set bgcolor, app title
    * Text color
    * Circle shape

--- src/javaFX2/TestFX.java
+++ src/javaFX2/TestFX.java
@@ -1,19 +1,35 @@
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TestFX extends Application {
 
     @Override
     public void start(Stage stage) {
+        Group root = new Group();
+        Scene scene = new Scene(root, 500, 300);
+        scene.setFill(Color.LIGHTPINK);
+
+        Circle circle = new Circle(60, 40, 30, Color.GREEN);
+
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("TEST, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Text text = new Text(10, 90, "Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        text.setFill(Color.DARKRED);
+
+        Font font = new Font(20);
+        text.setFont(font);
+
+        root.getChildren().add(circle);
+        root.getChildren().add(text);
         stage.setScene(scene);
         stage.show();
+        stage.setTitle("Application Title");
     }
 
     public static void main(String[] args) {
